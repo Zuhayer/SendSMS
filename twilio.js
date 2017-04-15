ce6bb87c2beceb7a895367beb54ed023d5e55895
Popesites <support@popesites.com>
2017-04-14 18:36:33 -0700

    First commit

--- twilio.js
+++ twilio.js
@@ -4,14 +4,14 @@ var authToken = 'f4f75e9d6e2298d720276f489aff387e';
 
 //require the Twilio module and create a REST client
 var client = require('twilio')(accountSid, authToken);
-var number = "+1"+document.getElementById('phonenumber');
+// var number = "+1"+document.getElementById('phonenumber');
 var body = "i love you";
 
 function sendMessage() {
     client.messages.create({
-        to: number,
-        from: "+12403033689",
-        body: body,
+        to: "+17034747206",
+        from: "+12403033886",
+        body: body
     }, function (err, message) {
         console.log(message.sid);
     });
